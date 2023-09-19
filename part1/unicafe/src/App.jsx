4b25caab50c8297b60f3d758f72e3b587003c142
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-09-20 00:49:21 +0300

    Exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,10 @@ const Button = ({ handleClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -24,12 +25,16 @@ const Statistics = ({ good, neutral, bad }) => {
         <div>No feedback given</div>
       ) : (
         <div>
-          <StatisticLine text='good' value={good} />
-          <StatisticLine text='neutral' value={neutral} />
-          <StatisticLine text='bad' value={bad} />
-          <StatisticLine text='all' value={all} />
-          <StatisticLine text='average' value={average} />
-          <StatisticLine text='positive' value={positive} />
+          <table>
+            <tbody>
+              <StatisticLine text='good' value={good} />
+              <StatisticLine text='neutral' value={neutral} />
+              <StatisticLine text='bad' value={bad} />
+              <StatisticLine text='all' value={all} />
+              <StatisticLine text='average' value={average} />
+              <StatisticLine text='positive' value={positive} />
+            </tbody>
+          </table>
         </div>
       )}
     </>
