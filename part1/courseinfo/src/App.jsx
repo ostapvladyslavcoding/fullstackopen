15771dfaf0d1bc556a1601e8fc7287c685c0090a
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-09-19 17:38:23 +0300

    Exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,11 +3,21 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  return <p>{props.part} {props.exercises}</p>
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
 }
 
 const Total = (props) => {
   return <p>Number of exercises {props.total}</p>
+}
+
+const Part = (props) => {
+  return <p>{props.part} {props.exercises}</p>
 }
 
 const App = () => {
@@ -22,9 +32,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
