e5630111874f0f417f4de5a1030224fdfc3a5609
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-09-21 02:23:19 +0300

    Exercise 2.1

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,28 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </>
+)
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
+
 const App = () => {
   const course = {
     id: 1,
