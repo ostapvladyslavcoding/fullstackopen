076fae6f0b3b101ca64b6404f80776bbfadd80ac
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-09-25 16:39:37 +0300

    Exercise 3.8

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const morgan = require('morgan')
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
+morgan.token('body', (req) => {
+  if (req.method === 'POST') return JSON.stringify(req.body)
+})
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
   {
