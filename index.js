2b4668db93cabfc666e2b9765756dbff4854a1cf
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-09-25 16:04:40 +0300

    Exercise 3.4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -38,7 +38,7 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
-app.get(`/api/persons/:id`, (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find((person) => person.id === id)
 
@@ -47,6 +47,13 @@ app.get(`/api/persons/:id`, (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  persons = persons.filter((person) => person.id !== id)
+  res.status(204).end()
 })
 
 const PORT = 3001
