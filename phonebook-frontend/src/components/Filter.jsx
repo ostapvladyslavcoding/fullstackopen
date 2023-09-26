58ae5e41fb1305f30ca464ac8b328250628b92ed
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-09-26 21:44:37 +0300

    Initial commit of directory restructure of part3

---
+++ phonebook-frontend/src/components/Filter.jsx
@@ -0,0 +1,13 @@
+const Filter = ({ filter, setFilter }) => {
+  return (
+    <div>
+      filter shown with
+      <input
+        value={filter}
+        onChange={({ target }) => setFilter(target.value)}
+      />
+    </div>
+  )
+}
+
+export default Filter
