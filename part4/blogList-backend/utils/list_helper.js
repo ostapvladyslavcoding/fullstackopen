1150e7271b6e54f896e5bda2f5e683f20fa6806e
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-10-02 23:38:07 +0300

    Exercise 4.5

--- part4/blogList-backend/utils/list_helper.js
+++ part4/blogList-backend/utils/list_helper.js
@@ -1,10 +1,12 @@
-const dummy = () => {
-  return 1
-}
+const dummy = () => 1
 
 const totalLikes = (blogs) => blogs.reduce((sum, cur) => sum + cur.likes, 0)
+
+const favoriteBlog = (blogs) =>
+  blogs.reduce((fav, cur) => (fav.likes > cur.likes ? fav : cur), blogs[0])
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
