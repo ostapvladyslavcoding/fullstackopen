d8e4a3832cd6ccd3dc8f26984af780d24b707443
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-10-02 20:23:22 +0300

    Exercise 3.22

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -34,7 +34,7 @@ if (process.argv.length === 5) {
     number: process.argv[4],
   })
 
-  person.save().then((result) => {
+  person.save().then(() => {
     console.log(`added ${person.name} number ${person.number} to phonebook`)
     mongoose.connection.close()
   })
