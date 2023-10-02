f9583ca643207ca38c44d370c2420cec2741866a
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-10-02 22:51:44 +0300

    Exercise 4.4

--- part4/blogList-backend/utils/list_helper.js
+++ part4/blogList-backend/utils/list_helper.js
@@ -1,7 +1,10 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => blogs.reduce((sum, cur) => sum + cur.likes, 0)
+
 module.exports = {
   dummy,
+  totalLikes,
 }
