52fa9f0be95021d5e4f59fadbd2f612812b58b04
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-10-02 22:07:31 +0300

    Exercise 4.2

---
+++ part4/blogList-backend/utils/middleware.js
@@ -0,0 +1,22 @@
+const logger = require('./logger')
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, req, res, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  unknownEndpoint,
+  errorHandler,
+}
