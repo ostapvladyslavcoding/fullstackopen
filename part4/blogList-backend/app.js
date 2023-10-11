03bcf87d0d9b41d420f57ce3e75c754e62cc2e29
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2023-10-12 00:44:49 +0300

    Exercise 4.15

--- part4/blogList-backend/app.js
+++ part4/blogList-backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -26,6 +27,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
