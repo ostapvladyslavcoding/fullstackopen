3468e4f9952dd433e24cd3cbad68617eaddabb29
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2024-03-07 22:49:36 +0200

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const hideWhenVisible = { display: detailsVisible ? 'none' : '' }
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  console.log(blog)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <div style={hideWhenVisible}>
+        <button onClick={() => setDetailsVisible(true)}>View</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>
+          likes: {blog.likes} <button>like</button>
+        </p>
+        <p>{blog.user.username}</p>
+        <button onClick={() => setDetailsVisible(false)}>Hide</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
