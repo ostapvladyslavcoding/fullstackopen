714dfdb9a56e349aa133dccb45426fb014caab71
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2024-03-10 17:13:59 +0200

    Exercise 5.17

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -115,6 +115,7 @@ const App = () => {
           <div>
             username
             <input
+              data-testid='username'
               type='text'
               value={username}
               name='Username'
@@ -124,6 +125,7 @@ const App = () => {
           <div>
             password
             <input
+              data-testid='password'
               type='password'
               value={password}
               name='Password'
---
+++ part5/e2e/tests/blog_app.spec.js
@@ -0,0 +1,21 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Test Name',
+        username: 'test',
+        password: 'test',
+      },
+    })
+
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByTestId('username')).toBeVisible()
+    await expect(page.getByTestId('password')).toBeVisible()
+  })
+})
