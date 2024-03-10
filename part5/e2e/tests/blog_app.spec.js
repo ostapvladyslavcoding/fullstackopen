054943d22c8fa92275e2b5ede5c4336b5be6bfc3
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2024-03-10 17:25:28 +0200

    Exercise 5.18

--- part5/e2e/tests/blog_app.spec.js
+++ part5/e2e/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -17,5 +18,21 @@ describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByTestId('username')).toBeVisible()
     await expect(page.getByTestId('password')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'test', 'test')
+      await expect(page.getByText('Test Name logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'test', 'wrong')
+      const errorDiv = await page.locator('.error')
+      await expect(errorDiv).toContainText('invalid username or password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(page.getByText('Test Name logged in')).not.toBeVisible()
+    })
   })
 })
