7f50532b371727ea0f7c639d876a13b4baef0bc6
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2024-03-12 21:12:24 +0200

    Exercise 6.12

---
+++ part6/redux-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux'
+// import { setNotification } from '../reducers/notificationReducer'
+
+const Notification = () => {
+  const notification = useSelector((state) => state.notification)
+  console.log(notification)
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+  }
+  return <div style={style}>{notification}</div>
+}
+
+export default Notification
