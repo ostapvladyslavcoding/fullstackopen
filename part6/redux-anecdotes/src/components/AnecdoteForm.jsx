31c473a68876071199f2f0ab01d35feabba8912a
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2024-03-12 23:58:33 +0200

    Exercise 6.15

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -4,19 +4,22 @@ import {
   removeNotification,
   setNotification,
 } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes.js'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
     dispatch(setNotification(`Created new anecdote: "${content}"`))
     setTimeout(() => {
       dispatch(removeNotification())
     }, 5000)
   }
+
   return (
     <>
       <h2>create new</h2>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,18 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
 const anecdotesSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        id: getId(),
-        votes: 0,
-      })
+      state.push(action.payload)
     },
     addVote(state, action) {
       const id = action.payload
@@ -26,7 +19,7 @@ const anecdotesSlice = createSlice({
       )
     },
     appendAnecdote(state, action) {
-      state.push(action.paylaod)
+      state.push(action.payload)
     },
     setAnecdotes(state, action) {
       return action.payload
