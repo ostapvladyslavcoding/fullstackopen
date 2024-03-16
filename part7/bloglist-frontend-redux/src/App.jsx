03a65a049441f482aef6f62c29b53b59ee3b785f
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2024-03-16 18:26:32 +0200

    Initial commit of bloglist-frontend-redux

---
+++ part7/bloglist-frontend-redux/src/App.jsx
@@ -0,0 +1,173 @@
+import { useEffect, useRef, useState } from 'react'
+import Blog from './components/Blog'
+import BlogForm from './components/BlogForm.jsx'
+import Togglable from './components/Togglable'
+import blogService from './services/blogs'
+import loginService from './services/login.js'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [info, setInfo] = useState({ message: null })
+
+  const blogFormRef = useRef()
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const infoMessage = (message, type = 'info') => {
+    setInfo({
+      message,
+      type,
+    })
+
+    setTimeout(() => {
+      setInfo({ message: null })
+    }, 5000)
+  }
+
+  const Notification = ({ info }) => {
+    if (!info.message) {
+      return
+    }
+
+    return <div className={info.type}>{info.message}</div>
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+
+      window.localStorage.setItem('loggedBloglistappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      infoMessage(`Logged in as "${user.name}"`)
+    } catch (error) {
+      console.error(error)
+      infoMessage(error.response.data.error, 'error')
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
+    infoMessage('Logged out')
+  }
+
+  const addBlog = async (blogObject) => {
+    try {
+      blogFormRef.current.toggleVisibility()
+      const newBlog = await blogService.create(blogObject)
+      setBlogs(blogs.concat(newBlog))
+
+      infoMessage(`Added "${blogObject.title}" by "${blogObject.author}"!`)
+    } catch (error) {
+      console.error(error)
+      infoMessage(error.response.data.error, 'error')
+    }
+  }
+
+  const updateLikes = async (id, updatedBlog) => {
+    try {
+      const res = await blogService.update(id, updatedBlog)
+
+      setBlogs(blogs.map((blog) => (blog.id === res.id ? res : blog)))
+      infoMessage(`Liked "${res.title}"!`)
+    } catch (error) {
+      console.error(error)
+      infoMessage(error.response.data.error, 'error')
+    }
+  }
+
+  const deleteBlog = async (id) => {
+    try {
+      await blogService.remove(id)
+      setBlogs(blogs.filter((blog) => id.toString() !== blog.id.toString()))
+      infoMessage('Deleted blog!')
+    } catch (error) {
+      console.error(error)
+      infoMessage(error.response.data.error, 'error')
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Notification info={info} />
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              data-testid='username'
+              type='text'
+              value={username}
+              name='Username'
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              data-testid='password'
+              type='password'
+              value={password}
+              name='Password'
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type='submit'>login</button>
+        </form>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <Notification info={info} />
+      <div style={{ display: 'inline' }}>{user.name} logged in</div>
+      <button onClick={handleLogout}>logout</button>
+
+      <Togglable
+        buttonLabel='new blog'
+        ref={blogFormRef}
+      >
+        <BlogForm createBlog={addBlog} />
+      </Togglable>
+
+      <h2>Blogs</h2>
+      <div data-testid='parent'>
+        {blogs
+          .sort((a, b) => b.likes - a.likes)
+          .map((blog) => (
+            <Blog
+              key={blog.id}
+              blog={blog}
+              updateLikes={updateLikes}
+              deleteBlog={deleteBlog}
+              currentUser={user.username}
+            />
+          ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ part7/bloglist-frontend-redux/src/components/Blog.jsx
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+const Blog = ({ blog, updateLikes, deleteBlog, currentUser }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const hideWhenVisible = { display: detailsVisible ? 'none' : '' }
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const handleLikeClick = () => {
+    const updatedBlog = {
+      user: blog.user.id,
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: blog.likes + 1,
+    }
+    updateLikes(blog.id, updatedBlog)
+  }
+
+  const handleDeleteClick = () => {
+    if (window.confirm(`Remove blog "${blog.title}" by "${blog.author}"?`))
+      deleteBlog(blog.id)
+  }
+
+  return (
+    <div
+      title='blog'
+      style={blogStyle}
+    >
+      <span>
+        {blog.title} {blog.author}
+      </span>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setDetailsVisible(true)}>View</button>
+      </div>
+      <div
+        className='togglableContent'
+        style={showWhenVisible}
+      >
+        <button onClick={() => setDetailsVisible(false)}>Hide</button>
+        <p>{blog.url}</p>
+        <p>
+          likes: {blog.likes} <button onClick={handleLikeClick}>like</button>
+        </p>
+        <p>{blog.user.username}</p>
+        {blog.user.username === currentUser && (
+          <button onClick={handleDeleteClick}>delete</button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+Blog.propTypes = {
+  blog: PropTypes.object.isRequired,
+  updateLikes: PropTypes.func.isRequired,
+  deleteBlog: PropTypes.func.isRequired,
+  currentUser: PropTypes.string.isRequired,
+}
+
+export default Blog
