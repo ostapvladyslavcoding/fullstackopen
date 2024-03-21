6492b9d2fc44cfdb6c310e434fc59182ae3164a1
Vladyslav Ostapchuk <ostapvladyslavcoding@gmail.com>
2024-03-21 05:24:37 +0200

    Exercise 8.6

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -101,6 +102,15 @@ type Query {
   allBooks(author: String, genre: String): [Book!]!
   allAuthors: [Author!]!
 }
+
+type Mutation {
+  addBook(
+    title: String!
+    published: Int!
+    author: String
+    genres: [String!]!
+  ): Book
+}
 `
 
 const resolvers = {
@@ -125,6 +135,16 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.filter((book) => book.author === root.name).length
+    },
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      if (!authors.find((author) => author.name === args.author)) {
+        authors = authors.concat({ name: args.author, id: uuid() })
+      }
+      return book
     },
   },
 }
